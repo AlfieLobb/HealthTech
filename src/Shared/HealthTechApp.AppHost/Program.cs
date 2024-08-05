e5c5814e00576b9eb5a173a6a08f897568d79465
Alfie Lobb <your.name@domain.com>
2024-08-05 03:23:56 +0100

    Added Blazor Web App

--- src/Shared/HealthTechApp.AppHost/Program.cs
+++ src/Shared/HealthTechApp.AppHost/Program.cs
@@ -2,4 +2,6 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Booking_Api>("booking-api");
 
+builder.AddProject<Projects.HealthTechApp_Web>("healthtechapp-web");
+
 builder.Build().Run();
