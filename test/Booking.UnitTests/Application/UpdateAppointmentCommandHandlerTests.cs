b3526733e748770d92406632ec825f7c684990c2
Alfie Lobb <your.name@domain.com>
2024-08-06 20:28:06 +0100

    Added UnitTest for example for Mediatr command UpdateAppointment.
    not fully verifying that everything updates just yet but bare bonse are in place

---
+++ test/Booking.UnitTests/Application/UpdateAppointmentCommandHandlerTests.cs
@@ -0,0 +1,60 @@
+﻿
+using Booking.Application.Commands;
+using Booking.Domain.AggregatesModel.AppointmentAggregate;
+using Booking.Domain.AggregatesModel.PatientAggregate;
+using Booking.UnitTests.Builders;
+using MassTransit;
+using MediatR;
+using Microsoft.Testing.Platform.Logging;
+using NSubstitute;
+using NUnit.Framework.Legacy;
+
+namespace Booking.UnitTests.Domain;
+
+[TestFixture]
+public class UpdateAppointmentCommandHandlerTests
+{
+
+
+    private readonly IAppointmentRepository _appointmentRepositoryMock;
+    private readonly Microsoft.Extensions.Logging.ILogger<UpdateAppointmentCommandHandler> _logger;
+    private readonly IPatientRepository _patientRepository;
+    private IPublishEndpoint _publishEndpoint;
+    public UpdateAppointmentCommandHandlerTests()
+    {
+        _appointmentRepositoryMock = Substitute.For<IAppointmentRepository>();
+        _logger = Substitute.For<Microsoft.Extensions.Logging.ILogger<UpdateAppointmentCommandHandler>>();
+        _patientRepository = Substitute.For<IPatientRepository>();
+        _publishEndpoint = Substitute.For<IPublishEndpoint>();
+    }
+
+    [Test]
+    public void UpdateCommand_Run_Success()
+    {
+        // Arrange
+        var updateAppointmentCommandHandler = new UpdateAppointmentCommandHandler(_appointmentRepositoryMock, _logger, _patientRepository, _publishEndpoint);
+
+        var appointmentId = 1;
+        var appointmentDate = DateTime.UtcNow;
+        var issue = "Issue New";
+        var email = "email@email.co.uk";
+        var name = "Some Name";
+        var contactNumber = "071231234124";
+        var updateCommand = new UpdateAppointmentCommand(appointmentId, appointmentDate, issue, email, name, contactNumber);
+
+        var existingAppointment = new AppointmentBuilder().BuildAppointment();
+        _appointmentRepositoryMock.GetAsync(appointmentId).Returns(existingAppointment);
+
+        // Act
+        var response = updateAppointmentCommandHandler.Handle(updateCommand, CancellationToken.None).Result;
+
+        // Assert
+        ClassicAssert.IsTrue(response);
+        ClassicAssert.AreEqual(appointmentDate, existingAppointment.GetAppointmentDate);
+        ClassicAssert.AreEqual(issue, existingAppointment.GetIssue);
+        ClassicAssert.IsNull(existingAppointment.GetApproverId);
+        ClassicAssert.IsNull(existingAppointment.GetApprovalDate);
+        //TODO Test Patient details update
+    }
+
+}
